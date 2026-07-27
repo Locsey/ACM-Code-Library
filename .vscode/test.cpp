3a921f891faa96aff9c425c58b8a8b15102de575
Locsey <3418474607@qq.com>
2026-07-27 18:58:30 +0800

    Initial commit: 搭建算法模板库基础结构

---
+++ .vscode/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    // 第6行：程序运行到这里会停下来，等待你在终端敲键盘
+    cin >> a >> b; 
+    
+    int sum = a + b; 
+    cout << sum << endl; 
+    return 0;
+} 
